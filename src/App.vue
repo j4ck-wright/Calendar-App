ca3869a44cf0b6bb63b02122c5e6f7ac25e02866
Jack Wright <jackw221001@gmail.com>
2022-07-22 19:53:07 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main class="mt-16 text-center text-blue-darken-3">
+      <h1>Hello World!</h1>
+      <p>Vite + Vue 3 + Vuetify 3 Beta</p>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  components: {
+    
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
