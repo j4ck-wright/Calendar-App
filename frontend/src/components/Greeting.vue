057cafd1b9f4ad9e508d7f74927c5e178152c39d
Jack Wright <jackw221001@gmail.com>
2022-08-17 20:25:29 +0100

    added files

---
+++ frontend/src/components/Greeting.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <!-- <span class="greeting">Good morning, </span><span class="user-name">Jack</span> -->
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
